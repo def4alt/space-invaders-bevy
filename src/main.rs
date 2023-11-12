a87cfbadbf357e3730c9640ecda91a094a5230ac
Andrii Olkhovych <the.def4alt@gmail.com>
2023-11-12 14:45:40 +0100

    Add player with movement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,46 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 fn main() {
-    App::new().add_systems(Startup, hello_world).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, player_movement)
+        .run();
 }
 
-fn hello_world() {
-    println!("Hello world!");
+#[derive(Component)]
+struct Player;
+
+fn setup(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn((
+        Player,
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(10.0).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+            ..Default::default()
+        },
+    ));
+}
+
+fn player_movement(
+    time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player_transform: Query<&mut Transform, With<Player>>,
+) {
+    let mut transform = player_transform.single_mut();
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        transform.translation.x -= 100. * time.delta_seconds();
+    }
+
+    if keyboard_input.pressed(KeyCode::Right) {
+        transform.translation.x += 100. * time.delta_seconds();
+    }
 }
